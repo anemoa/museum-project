982d3e8efc56befd025e9e3e8026d92551d714b0
anemoa <anemoia.yves@gmail.com>
2024-10-11 22:06:42 +0900

    feat: 작품소개 페이지 추가

--- src/components/PageSpecific/WorkOfArts/ArtsInfoBox.js
+++ src/components/PageSpecific/WorkOfArts/ArtsInfoBox.js
@@ -3,9 +3,12 @@ import React from 'react'
 const ArtsInfoBox = () => {
   return (
 		<div className='arts_info_box'>
-			arts_info_box
+			<div>
+				<p>Measurements</p>
+				
+			</div>
 		</div>
 	)
 }
 
-export default ArtsInfoBox
+export default ArtsInfoBox;
