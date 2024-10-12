7e0980214c0c7b79e292d9a9b427a9ead67fd5fc
anemoa <anemoia.yves@gmail.com>
2024-10-12 23:57:41 +0900

    feat: Place ArtsOfWork page add

--- src/components/PageSpecific/WorkOfArts/ArtsInfoBox.js
+++ src/components/PageSpecific/WorkOfArts/ArtsInfoBox.js
@@ -5,7 +5,17 @@ const ArtsInfoBox = () => {
 		<div className='arts_info_box'>
 			<div>
 				<p>Measurements</p>
-				
+				<p>133 155cm</p>
+			</div>
+
+			<div>
+				<p>Credit line</p>
+				<p>Purchased from John and Sunday Reed 1980</p>
+			</div>
+
+			<div>
+				<p>Copyright & credit</p>
+				<p>Estate of Mike Brown</p>
 			</div>
 		</div>
 	)
