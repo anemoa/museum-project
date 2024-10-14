6227581369301010e0197f12f163fb37655fea72
anemoa <anemoia.yves@gmail.com>
2024-10-14 22:15:25 +0900

    feat: Place ArtsOfWork page

--- src/pages/WorkOfArts.js
+++ src/pages/WorkOfArts.js
@@ -4,7 +4,7 @@ import ArtsWrap from '../components/PageSpecific/WorkOfArts/ArtsWrap';
 
 const WorkOfArts = () => {
   return (
-    <main className='container'>
+    <main className='work_of_arts_container'>
         <ArtsWrap />
     </main>
   );
