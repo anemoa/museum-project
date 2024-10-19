3cf0b8cb04eacac4bd8d44a6243170773e7ca013
anemoa <anemoia.yves@gmail.com>
2024-10-19 15:36:22 +0900

    feat: main 페이지 추가

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainVisual from '../components/PageSpecific/Main/MainVisual'
+
+const Main = () => {
+  return (
+    <main>
+        <MainVisual />
+    </main>
+  )
+}
+
+export default Main
