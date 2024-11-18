43c7c4794838c32b867ff2702836b8c647d623c2
anemoa <anemoia.yves@gmail.com>
2024-11-18 20:49:37 +0900

    feat: added style & change structure ArtsInfoBox component

--- src/components/PageSpecific/WorkOfArts/ArtsInfoBox.js
+++ src/components/PageSpecific/WorkOfArts/ArtsInfoBox.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const ArtsInfoBox = () => {
   return (
 		<div className='arts_info_box'>
-			<div>
-				<p>Measurements</p>
-				<p>133 155cm</p>
+
+			<div className='sub_info_box'>
+				<strong className='sub_info_title'>Measurements</strong>
+				<p className='sub_info_value'>133 x 155cm</p>
 			</div>
 
-			<div>
-				<p>Credit line</p>
-				<p>Purchased from John and Sunday Reed 1980</p>
+			<div className='sub_info_box'>
+				<strong className='sub_info_title'>Measurements</strong>
+				<p className='sub_info_value'>133 x 155cm</p>
 			</div>
 
-			<div>
-				<p>Copyright & credit</p>
-				<p>Estate of Mike Brown</p>
+			<div className='sub_info_box'>
+				<strong className='sub_info_title'>Measurements</strong>
+				<p className='sub_info_value'>133 x 155cm</p>
 			</div>
+
+			<div className='sub_info_box'>
+				<strong className='sub_info_title'>Measurements</strong>
+				<p className='sub_info_value'>133 x 155cm</p>
+			</div>
+
 		</div>
 	)
 }
--- src/components/PageSpecific/WorkOfArts/ArtsSubInfo.js
+++ src/components/PageSpecific/WorkOfArts/ArtsSubInfo.js
@@ -3,9 +3,11 @@ import React from 'react'
 const ArtsSubInfo = () => {
   return (
 		<div className='arts_sub_info'>
-			arts_sub_info
+			<div >
+				Artist
+			</div>
 		</div>
 	)
 }
 
-export default ArtsSubInfo
+export default ArtsSubInfo;
