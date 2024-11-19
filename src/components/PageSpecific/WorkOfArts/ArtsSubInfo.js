74a604aad2fff184d46c897ea062d8c426ead65f
anemoa <anemoia.yves@gmail.com>
2024-11-19 23:45:04 +0900

    feat: added ArtsSubInfo components  structure

--- src/components/PageSpecific/WorkOfArts/ArtsSubInfo.js
+++ src/components/PageSpecific/WorkOfArts/ArtsSubInfo.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ArtsSubInfo = () => {
   return (
 		<div className='arts_sub_info'>
 			<div >
-				Artist
+				<p>Artist</p>
+				<Link>Artist name</Link>
+			</div>
+
+			<div>
+				<p>Add List</p>
+				<p>icon</p>
 			</div>
 		</div>
 	)
